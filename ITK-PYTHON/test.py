1028d4387fb18721ced7b09e26f0fc8c03382a88
ManojKRISHNAPPA <contactmanojmech@gmail.com>
2026-07-30 19:05:47 +0530

    updated the notes

---
+++ ITK-PYTHON/test.py
@@ -0,0 +1,3 @@
+from package.calculator import add
+
+print(add(2,4))
---
+++ test-application/filehandling/main.py
@@ -0,0 +1,7 @@
+print("="*50)
+print("Student Login")
+print("="*50)
+
+student = input("Enter your name: ")
+print(f"Welcome {student}")
+print(f"Program finsihed")
