0ae695b07298814c69b456ba2e3917da36904ab2
ManojKRISHNAPPA <contactmanojmech@gmail.com>
2026-08-12 15:59:22 +0530

    updated the typing

--- test.py
+++ test.py
@@ -1,13 +1,3 @@
-player_name = "Virat Kohli"
-age = 35
-batting_avg = 58.07
-is_captain = False
-jersy_name = 18
-
-
-name: str = "Manoj"
-age: int = 30
-salary: float = 50000
-is_admin: bool = True
-
-
+def greet(name: str) -> str:
+    return f"Hello {name}"
+greet(110)
